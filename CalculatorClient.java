36109432e95bbcf62d28df352222b794246fc0c1
JKriko <91527042+JKriko@users.noreply.github.com>
2022-02-22 19:49:18 +0200

    Update CalculatorClient.java

--- CalculatorClient.java
+++ CalculatorClient.java
@@ -6,10 +6,10 @@ public class CalculatorClient {
 public static void main(String[] args) {
 try {
 Calculator c = (Calculator) Naming.lookup( "rmi://localhost/CalculatorService");
-System.out.println( c.sub(4, 3) );
-System.out.println( c.add(4, 5) );
-System.out.println( c.mul(3, 6) );
-System.out.println( c.div(9, 3) );
+System.out.println( c.sub(1, 5) );
+System.out.println( c.add(4, 7) );
+System.out.println( c.mul(4, 9) );
+System.out.println( c.div(6, 3) );
 }
 catch (MalformedURLException murle) { System.out.println( "MalformedURLException"+ murle); }
 catch (RemoteException re) { System.out.println("RemoteException"+re); }
